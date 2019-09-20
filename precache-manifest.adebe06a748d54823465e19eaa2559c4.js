b2a963f4507657507f548ee1846fdfcef2cc23b0
Mauro Martinez <mmartinez@edrans.com>
2019-09-19 22:08:43 -0300

    Updates

---
+++ precache-manifest.adebe06a748d54823465e19eaa2559c4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a46b9d5a6f18324522fc174a6a89a72",
+    "url": "/busca-letras/index.html"
+  },
+  {
+    "revision": "2682cf7591f5936ef239",
+    "url": "/busca-letras/static/css/main.0d7ad5ad.chunk.css"
+  },
+  {
+    "revision": "d354b559109966ce012e",
+    "url": "/busca-letras/static/js/2.c9b0a1e0.chunk.js"
+  },
+  {
+    "revision": "2682cf7591f5936ef239",
+    "url": "/busca-letras/static/js/main.d5bd9025.chunk.js"
+  },
+  {
+    "revision": "94262c28d4a0947a9995",
+    "url": "/busca-letras/static/js/runtime-main.3bc71eea.js"
+  }
+]);
